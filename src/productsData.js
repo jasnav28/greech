408c73d34e4aa50aca15d99f300e4f10d1ce639a
Jashwanth5102495 <112746469+jashwanth5102495@users.noreply.github.com>
2026-06-20 23:43:30 +0530

    Update products data

--- src/productsData.js
+++ src/productsData.js
@@ -14,64 +14,64 @@ export const PRODUCTS = [
       '(iii) Specific Gravity 1.0'
     ] 
   },
-  { 
-    brand: 'Red Gram', 
-    specification: 'Potassium Humate 49% (Powder)', 
+  {
+    brand: 'Red Gram',
+    specification: 'Potassium Humate 49% (Powder)',
     category: 'Humic Acid and Fulvic Acid and their Derivatives',
-    crops: ['Paddy', 'Tomato'], 
-    dosage: ['Paddy: One Soil application at 1 kg/ha.', 'Tomato: One Soil application at 1 kg/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E).', 
+    crops: ['Paddy', 'Tomato'],
+    dosage: ['Paddy: One Soil application at 1 kg/ha.', 'Tomato: One Soil application at 1 kg/ha.'],
+    gazette: '16th February, 2026 S.O. 876(E).',
     composition: [
-      '(i) Potassium humate (Source: Leonardite) per cent. by weight, minimum 49', 
+      '(i) Potassium humate (Source: Leonardite) per cent. by weight, minimum 49',
       '(ii) Silwet power (adjuvant) per cent. by weight, maximum 0.5',
       '(iii) Carboxymethyl cellulose per cent. by weight, maximum 1.0',
       '(iv) Maltodextrin powder per cent. by weight QS',
       '(v) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Flower', 
-    specification: 'Humates (12.5%) (Liquid)', 
+  {
+    brand: 'Flower',
+    specification: 'Humates (12.5%) (Liquid)',
     category: 'Humic Acid and Fulvic Acid and their Derivatives',
-    crops: ['Green gram'], 
-    dosage: ['Two soil applications at 5 litre/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E).', 
+    crops: ['Green gram'],
+    dosage: ['Two soil applications at 5 litre/ha'],
+    gazette: '16th February, 2026 S.O. 876(E).',
     composition: [
-      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 10.0', 
+      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 10.0',
       '(ii) Fulvic acid (Source: American Leonardite) per cent. by weight, minimum 2.5',
       '(iii) Tetrapotassium pyrophosphate per cent. by weight, minimum 0.9',
       '(iv) Potassium hydroxide 50% solution per cent. by weight, minimum 10.0',
       '(v) Alcoxylated fatty acid ester on vegetable base per cent. by weight, minimum 0.7',
       '(vi) Water per cent. by weight QS',
       '(vii) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Pushpak Gold', 
-    specification: 'Humates (12.5%) (Liquid)', 
+  {
+    brand: 'Pushpak Gold',
+    specification: 'Humates (12.5%) (Liquid)',
     category: 'Humic Acid and Fulvic Acid and their Derivatives',
-    crops: ['Green gram'], 
-    dosage: ['Two soil applications at 5 litre/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E).', 
+    crops: ['Green gram'],
+    dosage: ['Two soil applications at 5 litre/ha'],
+    gazette: '16th February, 2026 S.O. 876(E).',
     composition: [
-      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 10.0', 
+      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 10.0',
       '(ii) Fulvic acid (Source: American Leonardite) per cent. by weight, minimum 2.5',
       '(iii) Tetrapotassium pyrophosphate per cent. by weight, minimum 0.9',
       '(iv) Potassium hydroxide 50% solution per cent. by weight, minimum 10.0',
       '(v) Alcoxylated fatty acid ester on vegetable base per cent. by weight, minimum 0.7',
       '(vi) Water per cent. by weight QS',
       '(vii) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Chinna', 
-    specification: 'Humic acid 51 % (Granular)', 
+  {
+    brand: 'Chinna',
+    specification: 'Humic acid 51 % (Granular)',
     category: 'Humic Acid and Fulvic Acid and their Derivatives',
-    crops: ['Green gram'], 
-    dosage: ['Two soil applications at 1.5 kg/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E).', 
+    crops: ['Green gram'],
+    dosage: ['Two soil applications at 1.5 kg/ha.'],
+    gazette: '16th February, 2026 S.O. 876(E).',
     composition: [
-      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 51.00', 
+      '(i) Humic acid (Source: American Leonardite) per cent. by weight, minimum 51.00',
       '(ii) Fulvic acid per cent. by weight, minimum 10.00',
       '(iii) Potassium as K2O per cent. by weight, minimum 16.00',
       '(iv) Tetrapotassium pyrophosphate per cent. by weight, minimum 4.50',
@@ -79,84 +79,84 @@ export const PRODUCTS = [
       '(vi) Alcoxylated fatty acid ester on vegetable base per cent. by weight, minimum 0.36',
       '(vii) Water per cent. by weight, maximum 3.92',
       '(viii) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Advin', 
-    specification: 'Sargassum tenerrimum 10% (Liquid)', 
+  {
+    brand: 'Advin',
+    specification: 'Sargassum tenerrimum 10% (Liquid)',
     category: 'Seaweed Extract',
-    crops: ['Tomato', 'Rice'], 
-    dosage: ['Rice: Two foliar applications at 1000 ml/ha.', 'Tomato: Two foliar applications at 625 ml/ha.'], 
-    gazette: '13th August, 2025 S.O. 3741(E).', 
+    crops: ['Tomato', 'Rice'],
+    dosage: ['Rice: Two foliar applications at 1000 ml/ha', 'Tomato: Two foliar applications at 625 ml/ha'],
+    gazette: '13th August, 2025 S.O. 3741(E).',
     composition: [
-      '(i) Seaweed extract (Sargassum tenerrimum) per cent. by weight, minimum 10', 
+      '(i) Seaweed extract (Sargassum tenerrimum ) per cent. by weight, minimum 10',
       '(ii) Carbohydrates per cent. by weight, minimum 8',
       '(iii) Octylphenol ethoxylate as adjuvant per cent. by weight, minimum 3.1',
       '(iv) Sodium benzoate per cent. by weight, minimum 1.0',
       '(v) Water per cent. by weight QS',
       '(vi) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Swarna', 
-    specification: 'Sargassum tenerrimum 2%( Granular)', 
+  {
+    brand: 'Swarna',
+    specification: 'Sargassum tenerrimum 2%( Granular)',
     category: 'Seaweed Extract',
-    crops: ['Paddy', 'Tomato'], 
-    dosage: ['Paddy: One soil application at 12.5 kg/ha.', 'Tomato: One soil application at 12.5 kg/ha.'], 
-    gazette: '', 
+    crops: ['Paddy', 'Tomato'],
+    dosage: ['Paddy: One soil application at 12.5 kg/ha.', 'Tomato: One soil application at 12.5 kg/ha.'],
+    gazette: '',
     composition: [
-      '(i) Seaweed (Sargassum tenerrimum) extract per cent. by weight, minimum 2.0', 
+      '(i) Seaweed (Sargassum tenerrimum) extract per cent. by weight, minimum 2.0',
       '(ii) Phytase as adjuvant per cent. by weight, maximum 1.0',
       '(iii) Bentonite per cent. by weight, maximum 87',
       '(iv) Moisture per cent. by weight, maximum 10',
       '(v) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Kavacha', 
-    specification: 'Ascophyllum nodosum 15% (Liquid)', 
+  {
+    brand: 'Kavacha',
+    specification: 'Ascophyllum nodosum 15% (Liquid)',
     category: 'Seaweed Extract',
-    crops: ['Green gram'], 
-    dosage: ['Two foliar applications at 1500 ml/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E)', 
+    crops: ['Green gram'],
+    dosage: ['Two foliar applications at 1500 ml/ha'],
+    gazette: '16th February, 2026 S.O. 876(E)',
     composition: [
-      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 15', 
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 15',
       '(ii) Potassium sorbate per cent. by weight, minimum 0.28',
       '(iii) Citric acid per cent. by weight, minimum 1.91',
       '(iv) Water per cent. by weight, maximum 82.81',
       '(v) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Red Mite', 
-    specification: 'Sargassum tenerrimum 10% (II) (liquid)', 
+  {
+    brand: 'Red Mite',
+    specification: 'Sargassum tenerrimum 10% (II) (liquid)',
     category: 'Seaweed Extract',
-    crops: ['Chilli'], 
-    dosage: ['Three foliar applications at 1500 ml/ha.'], 
-    gazette: '13th August, 2025 S.O. 3741(E).', 
+    crops: ['Chilli'],
+    dosage: ['Three foliar applications at 1500 ml/ha'],
+    gazette: '13th August, 2025 S.O. 3741(E).',
     composition: [
-      '(i) Seaweed (Sargassum tenerrimum) extract per cent. weight by volume, minimum 10', 
+      '(i) Seaweed (Sargassum tenerrimum) extract per cent. weight by volume, minimum 10',
       '(ii) Formalin per cent. weight by volume, minimum 0.5',
       '(iii) Polysorbate 80 per cent. weight by volume, minimum 0.5',
       '(iv) Water per cent. weight by volume 89',
       '(v) Total (per cent.) 100'
-    ] 
+    ]
   },
-  { 
-    brand: 'Green Max', 
-    specification: 'Mixture of Humic Acid, Amino Acid, Vitamins and Bio-chemicals (powder)', 
+  {
+    brand: 'Green Max',
+    specification: 'Mixture of Humic Acid, Amino Acid, Vitamins and Bio-chemicals (powder)',
     category: 'Mixed Formulation',
-    crops: ['Okra'], 
-    dosage: ['Two soil applications at 250g/ha.'], 
-    gazette: '16th February, 2026 S.O. 876(E)', 
+    crops: ['Okra'],
+    dosage: ['Two soil applications at 250g/ha'],
+    gazette: '16th February, 2026 S.O. 876(E)',
     composition: [
-      '(i) Potassium humate per cent. by weight, minimum 39', 
+      '(i) Potassium humate per cent. by weight, minimum 39',
       '(ii) Amino acid per cent. by weight, minimum 10',
       '(iii) Vitamins per cent. by weight, minimum 22',
       '(iv) Sugar Alcohols (myo inositol) per cent. by weight, maximum 4',
       '(v) Modified starch per cent. by weight, maximum 25',
       '(vi) Total (per cent.) 100'
-    ] 
+    ]
   }
 ];
 
